7fdd7208b756de1891218a2c72b5d28ce90dc288
Kunal Sharma <85027477+Venom9182@users.noreply.github.com>
2024-10-17 19:22:24 +0530

    Create shorttemplates.cpp

---
+++ shorttemplates.cpp
@@ -0,0 +1,12 @@
+#define v vector
+#define pb push_back
+#define vv vector<vector<int>>
+#define pii pair<int, int>
+#define mp make_pair
+#define umap unordered_map
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define fore(i, a, b) for(int i = a; i <= b; i++)
+#define debug(x) cout << #x << ": " << x << endl;
+#define maxele *max_element(arr.begin(), arr.end())  // Maximum element in a vector
+#define minele *min_element(arr.begin(), arr.end())  // Minimum element in a vector
+#define gcd __gcd  // GCD function
